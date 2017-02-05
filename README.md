42fb5be1bc1f0de5d814c6fc3511f7dd85482a8c
Tiemen <Tiemen2@live.nl>
2017-02-05 01:01:11 +0100

    Updated files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Spambot
-Discord.js Bot that will handle spam
+A Discord.js bot that handles mention spam, embed spam, image spam
+
+Inspired by [No Mention-Spam 2.0](https://github.com/eslachance/nms2)
+
+###Contact me:
+Discord: `Tiemen#0107`
+Twitter: [@ThaTiemsz](https://twitter.com/ThaTiemsz)
--- spam.js
+++ spam.js
@@ -7,64 +7,80 @@ client.on("ready", () => {
 	console.log("I'm ready to do work!");
 });
 
-const slowmode = new Map();
+const slowmode_mentions = new Map();
+const slowmode_links = new Map();
+const slowmode_attachments = new Map();
 const ratelimit = 7500; // within 7.5s
 const guild = "209719953706844161"; // guild id
 const logChannel = "274871132497379328"; // logs channel id
 
-client.on("message", msg => {
+client.on("message", message => {
 
-	if (msg.content.startsWith("!ping")) {
+	if (message.content.startsWith("!ping")) {
 		let startTime = Date.now();
-		msg.channel.sendMessage("Ping...").then(newMessage => {
+		message.channel.sendMessage("Ping...").then(newMessage => {
 			let endTime = Date.now();
 			newMessage.edit("Pong! Took `" + Math.round(endTime - startTime) + "ms`!");
 		});
 	}
 
-	function log(logmsg) {
-		if (msg.guild.channels.has(logChannel)) {
-			msg.guild.channels.get(logChannel).sendEmbed(logmsg).then().catch(err => console.log(err));
+	function log(logmessage) {
+		if (message.guild.channels.has(logChannel)) {
+			message.guild.channels.get(logChannel).sendEmbed(logmessage).then().catch(err => console.log(err));
 		}
 	}
 
-	let banLevel = 1;
+	// set the max mentions/links/attachments that are allowed
+	let banLevel = {
+		"mentions": 10,
+		"links": 10,
+		"attachments": 10
+	};
 
-	// Ignore DMS, Webhooks, Mods, and break if no perms
-	if (msg.author.bot || !msg.guild || !msg.member || !msg.guild.member(client.user).hasPermission("BAN_MEMBERS") || msg.member.hasPermission("MANAGE_MESSAGES")) return;
+	// Ignore bots, DMs, Webhooks, if this bot has no perms, and Mods
+	if (message.author.bot || !message.guild || !message.member || !message.guild.member(client.user).hasPermission("BAN_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES")) return;
 
 	// Ignore if 1 mention and it's a bot (bot interaction)
-	if (msg.mentions.users.size == 1 && msg.mentions.users.first().bot) return;
+	if (message.mentions.users.size == 1 && message.mentions.users.first().bot) return;
 
-	// If there is no trace of the author in the slowmode map, add him.
-	let entry = slowmode.get(msg.author.id);
-	if (!entry) {
+	// If there is no trace of the author in the slowmode map, add them.
+	let entry_mentions = slowmode_mentions.get(message.author.id);
+	if (!entry_mentions) {
 		entry = 0;
-		slowmode.set(msg.author.id, entry);
+		slowmode_mentions.set(message.author.id, entry_mentions);
 	}
 
-	// Count BOTH user and role mentions
-	entry += msg.mentions.users.size + msg.mentions.roles.size;
-	console.log(entry);
-	console.log(slowmode);
+	let entry_links = slowmode_links.get(message.author.id);
+	if (!entry_links) {
+		entry = 0;
+		slowmode_links.set(message.author.id, entry_links);
+	}
+
+	let entry_attachments = slowmode_attachments.get(message.author.id);
+	if (!entry_attachments) {
+		entry = 0;
+		slowmode_attachments.set(message.author.id, entry_attachments);
+	}
+
+	// Count BOTH unique user and role mentions
+	entry_mentions += message.mentions.users.size + message.mentions.roles.size;
+	entry_links += message.embeds.size;
+	entry_attachments += message.attachments.size;
 
 	// If the total number of mentions in the last `ratelimit` is above the server ban level... well, ban their ass.
-	if (entry > banLevel) {
-		log(new Discord.RichEmbed().setTitle(':warning: Caution').setColor(0xFF8000).setTimestamp().addField('User', `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`).addField('Reason', `Spamming mentions x${entry}`));
+	if (entry_mentions > banLevel.mentions) {
+		message.member.ban(1).then(member => {
+			message.channel.sendMessage(`:ok_hand: banned \`${message.author.username}#${message.author.discriminator}\` for \`mention spam\``);
 
-		msg.member.ban(1)
-		.then(member => {
-			msg.channel.sendMessage(`:ok_hand: banned \`${msg.author.username}#${msg.author.discriminator}\` for \`spam\``);
-
-			log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`).addField('Reason', `Mentioning too many users (${entry})`));
+			log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.username}#${message.author.discriminator} (${message.author.id})`).addField('Reason', `Mentioning too many users (${entry}x)`));
 		})
 		.catch(e => {
-			log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
+			log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.username}#${message.author.discriminator} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
 		});
 	} else {
 		setTimeout(()=> {
-			entry -= msg.mentions.users.size + msg.mentions.roles.size;
-			if(entry <= 0) slowmode.delete(msg.author.id);
+			entry -= message.mentions.users.size + message.mentions.roles.size;
+			if(entry <= 0) slowmode.delete(message.author.id);
 		}, ratelimit);
 	}
 
